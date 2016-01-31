7e5d14e5097709c268e0f7d75bb8b26eafbf68d2
Alex Myasoedov <msoedov@gmail.com>
2016-01-31 22:14:55 +0200

    Get version from hex package manager

--- lib/mix_install.ex
+++ lib/mix_install.ex
@@ -1,7 +1,38 @@
-defmodule MixInstall.Install do
+defmodule Mix.Tasks.Install do
   use Mix.Task
+  @shortdoc """
+  TBD
+  """
 
-  def run(_) do
-    Mix.shell.info "hello"
+  @moduledoc """
+  """
+
+  @spec get_version(String.t, String.t) :: String.t
+  def get_version(data, package) do
+    lines = String.split(data, "\n")
+    line? = Enum.filter(lines, &String.starts_with?(&1, package) )
+    packages = Enum.map(line?, fn arg ->
+      [name, version] = String.split(arg, ~r{\s+})
+      {name, version}
+    end)
+    matched = Enum.filter(packages, fn {name, version} ->
+       name == package
+    end)
+    case matched do
+      [] -> :error
+      [{name, version}] -> {:ok, version}
+    end
+  end
+
+  def run(package) do
+     package
+     |> inspect
+     |> IO.puts
+     Mix.shell.info "rly? #{package}"
+
+     packages = Mix.Task.run "hex.search", [package]
+     get_version(packages, package)
+
+     Mix.Task.run "deps.get", []
   end
 end
--- test/mix_install_test.exs
+++ test/mix_install_test.exs
@@ -1,8 +1,23 @@
 defmodule MixInstallTest do
   use ExUnit.Case
-  doctest MixInstall
+  alias Mix.Tasks.Install
+
+  doctest Install
 
   test "the truth" do
     assert 1 + 1 == 2
+  end
+
+  test "Extarct package version" do
+    data = """
+    ar2ecto                0.1.2
+    arc_ecto               0.3.1
+    calecto                0.5.0
+    comeonin_ecto_password 0.0.3
+    couchdb_connector      0.2.0
+    ecto                   1.1.3
+    ecto_audit             0.0.1
+    """
+     assert {:ok, "1.1.3"} == Install.get_version(data, "ecto")
   end
 end
